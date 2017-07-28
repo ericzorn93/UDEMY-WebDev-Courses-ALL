5681ca7b6fd48c53ffbb244bc413c74293877052
Eric Zorn <goldencatch93@gmail.com>
2017-07-28 13:49:28 -0400

    IFFEE wrapped and questions working

--- JavaScript Course/Module 5/5-Advanced-JS/script.js
+++ JavaScript Course/Module 5/5-Advanced-JS/script.js
@@ -329,30 +329,64 @@ console.log(fullJapan); //Age Testing for Japan */
 
 
 
-//Coding Challenge v4 Game
+//Coding Challenge v4 Game USING PROTOTYPES
+
+//IFFE statement
+(function () {
+    function Question(question, answers, correct) {
+        // "use strict";
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
 
 
-function Question(question, answers, correct) {
-    "use strict";
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
+//Question Method
+    Question.prototype.displayQuestion = function () {
+        var i;
+        console.log(this.question);
 
-var q1 = new Question('Is JavaScript the Coolest programming language in the world?', ['Yes', 'No'], 0);
+        for (i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
 
-var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+    Question.prototype.checkAnswer = function (ans) {
+        if (ans === this.correct) {
+            console.log('Correct Answer!');
+        } else {
+            console.log('Wrong answer, try again!');
+        }
+    }
 
-var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+    var q1 = new Question('Is JavaScript the Coolest programming language in the world?', ['Yes', 'No'], 0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+
+    var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
 
 
 
 //Questions
-var questions = [q1, q2, q3];
+    var questions = [q1, q2, q3];
 
-var n = Math.floor(Math.random() * questions.length);
+    var n = Math.floor(Math.random() * questions.length);
 
-console.log(n);
+    questions[n].displayQuestion();
+
+    var answer  = parseInt(prompt('Please select the correct answer.'));
+
+    questions[n].checkAnswer(answer);
+
+})();
+
+
+
+
+
+
+
 
 
 
