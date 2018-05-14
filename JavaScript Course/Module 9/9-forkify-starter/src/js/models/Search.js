4c18d3903ce0f9cdffbc1f2028820c2581f0459b
Eric Zorn <goldencatch93@gmail.com>
2018-05-14 00:58:11 -0400

    starting recipe view

---
+++ JavaScript Course/Module 9/9-forkify-starter/src/js/models/Search.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        // const secondProxy = "https://cors-proxy.htmldriven.com/?url=";
+        const key = "66a021708a848799157dcccdd9ef124f";
+        
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
