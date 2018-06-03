31499d8c05d60bd3f8b9c43aca413d838c90a8b2
Eric Zorn <goldencatch93@gmail.com>
2018-06-03 00:16:09 -0400

    Increase and decrease servings

--- JavaScript Course/Module 9/9-forkify-starter/src/js/views/recipeView.js
+++ JavaScript Course/Module 9/9-forkify-starter/src/js/views/recipeView.js
@@ -62,12 +62,12 @@ export const renderRecipe = recipe => {
                     <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-decrease">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-minus"></use>
                             </svg>
                         </button>
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-increase">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-plus"></use>
                             </svg>
@@ -120,4 +120,15 @@ export const renderRecipe = recipe => {
 
     //  Insert HTML into Document
     elements.recipe.insertAdjacentElement('afterbegin', markupElement);
+};
+
+export const updateServingsIngredients = recipe => {
+    // Update counts
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+    // Update ingredients
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+       el.textContent = formatCount(recipe.ingredients[i].count);
+    });
 };
