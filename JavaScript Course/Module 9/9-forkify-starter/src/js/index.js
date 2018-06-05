ddd1ccfdd4094d1da824c641f05a8f039022ceb3
Eric Zorn <goldencatch93@gmail.com>
2018-06-05 00:17:58 -0400

    Persist Likes with localStorage API

--- JavaScript Course/Module 9/9-forkify-starter/src/js/index.js
+++ JavaScript Course/Module 9/9-forkify-starter/src/js/index.js
@@ -148,10 +148,6 @@ elements.shopping.addEventListener('click', e => {
 **** LIKE CONTROLLER
  */
 
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
   const currentID = state.recipe.id;
@@ -187,6 +183,20 @@ const controlLike = () => {
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore Likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 
